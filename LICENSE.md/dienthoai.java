a46fed3603fd150d16b2d783f3b8240204ae4bcd
ndhung18it2 <44264874+ndhung18it2@users.noreply.github.com>
2018-10-21 02:32:24 +0700

    Update and rename LICENSE.md to dienthoai.java

---
+++ LICENSE.md/dienthoai.java
@@ -0,0 +1,14 @@
++package baitapso1;
+public class dienthoai {
+ String mahang;
+ String tenhang; 
+ static long dongia;
+ static int soluong;
+public dienthoai() {}
+public dienthoai(String mahang,String tenhang,long dongia,int soluong) {
+	this.dongia=dongia;
+	this.mahang=mahang;
+	this.soluong=soluong;
+	this.tenhang=tenhang;
+   }
+}
