de7e0a9ee9412f6cdf46ceb2481a95b144f9af11
ndhung18it2 <44264874+ndhung18it2@users.noreply.github.com>
2018-10-25 11:23:11 +0700

    Update and rename dienthoai.java to Dienthoai.java

---
+++ LICENSE.md/Dienthoai.java
@@ -0,0 +1,15 @@
+package baitapso1;
+
+public class Dienthoai {
+	 String mahang;
+	 String tenhang; 
+	 static long dongia;
+	 static int soluong;
+	public Dienthoai() {}
+	public Dienthoai(String mahang,String tenhang,long dongia,int soluong) {
+		this.dongia=dongia;
+		this.mahang=mahang;
+		this.soluong=soluong;
+		this.tenhang=tenhang;
+	   }
+	}
